51b8ffb6879fc5f2f953d7437c703255996b2f00
kimdonghyeon <sh2115521@naver.com>
2022-05-12 18:04:02 +0900

    백준 11055 - dp

--- src/Main.java
+++ src/Main.java
@@ -16,37 +16,41 @@ public class Main {
         arr = new int[N];
         dp = new Integer[N];
 
-        for(int i = 0 ; i < N ; i++){
+        for(int i = 0; i < N ; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0] = 1;
+        dp[0] = arr[0];
 
         for(int i = 1 ; i < N ; i++){
-            down(i);
+            up(i);
         }
 
         int max = Integer.MIN_VALUE;
-        for(int i = 0 ; i< N ; i++){
+
+        for(int i = 0 ; i < N ; i++){
             max = Math.max(dp[i], max);
         }
 
         System.out.println(max);
 
     }
-    public static int down(int n){
+    public static int up(int n){
 
         if(dp[n] == null){
             dp[n] = 1;
 
             for(int i = 0 ; i < n ; i++){
-                if(arr[n] < arr[i]){
-                    dp[n] = Math.max(dp[n], down(i) + 1);
+                if(arr[n] > arr[i]){
+                    dp[n] = Math.max(dp[n], up(i) + arr[n]);
+                }else{
+                    dp[n] = Math.max(dp[n],arr[n]);
                 }
             }
+
         }
+
 
         return dp[n];
     }
-
 }
