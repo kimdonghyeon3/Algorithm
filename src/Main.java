ff07ca693281f7660043ffa294f4f9d917604c0a
kimdonghyeon <sh2115521@naver.com>
2022-05-12 18:02:36 +0900

    백준 11722 - dp

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,52 @@
 import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
+
+    static int[] arr;
+    static Integer[] dp;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        arr = new int[N];
+        dp = new Integer[N];
+
+        for(int i = 0 ; i < N ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        dp[0] = 1;
+
+        for(int i = 1 ; i < N ; i++){
+            down(i);
+        }
+
+        int max = Integer.MIN_VALUE;
+        for(int i = 0 ; i< N ; i++){
+            max = Math.max(dp[i], max);
+        }
+
+        System.out.println(max);
 
     }
+    public static int down(int n){
+
+        if(dp[n] == null){
+            dp[n] = 1;
+
+            for(int i = 0 ; i < n ; i++){
+                if(arr[n] < arr[i]){
+                    dp[n] = Math.max(dp[n], down(i) + 1);
+                }
+            }
+        }
+
+        return dp[n];
+    }
+
 }
