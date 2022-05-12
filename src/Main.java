3d3de7bdad6409fa4b6f105277839689887970db
kimdonghyeon <sh2115521@naver.com>
2022-05-12 18:27:20 +0900

    백준 11054(가장 긴 바이토닉 부분 수열, dp)

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     static int[] arr;
     static Integer[] dp;
+    static Integer[] re_dp;
 
     public static void main(String[] args) throws Exception {
 
@@ -15,26 +16,52 @@ public class Main {
 
         arr = new int[N];
         dp = new Integer[N];
+        re_dp = new Integer[N];
 
-        for(int i = 0; i < N ; i++) {
+        for(int i = 0 ; i < N ; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        dp[0] = arr[0];
+        dp[0] = 1;
+        re_dp[re_dp.length-1] = 1;
 
-        for(int i = 1 ; i < N ; i++){
+        for(int i = 1 ; i < N ;i++){
             up(i);
+            re_up(N-i);
         }
+        re_up(0);
+
+//        for(Integer a : dp){
+//            System.out.print(a + " ");
+//        }
+//        System.out.println();
+//        for(Integer a : re_dp){
+//            System.out.print(a + " ");
+//        }
 
         int max = Integer.MIN_VALUE;
-
         for(int i = 0 ; i < N ; i++){
-            max = Math.max(dp[i], max);
+            max = Math.max(max, dp[i] + re_dp[i]);
         }
 
-        System.out.println(max);
+        System.out.println(max-1);
 
     }
+    public static int re_up(int n){
+
+        if(re_dp[n] == null){
+            re_dp[n] = 1;
+
+            for(int i = n+1 ; i < re_dp.length ; i++){
+                if(arr[n] > arr[i]){
+                    re_dp[n] = Math.max(re_dp[n], re_up(i) + 1);
+                }
+            }
+        }
+
+        return re_dp[n];
+    }
+
     public static int up(int n){
 
         if(dp[n] == null){
@@ -42,15 +69,12 @@ public class Main {
 
             for(int i = 0 ; i < n ; i++){
                 if(arr[n] > arr[i]){
-                    dp[n] = Math.max(dp[n], up(i) + arr[n]);
-                }else{
-                    dp[n] = Math.max(dp[n],arr[n]);
+                    dp[n] = Math.max(dp[n], up(i) + 1);
                 }
             }
-
         }
-
 
         return dp[n];
     }
+
 }
