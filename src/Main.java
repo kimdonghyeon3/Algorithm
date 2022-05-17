4a724ef4a9b91dd4eba88f8ff2b7ccc9ee9664f7
kimdonghyeon <sh2115521@naver.com>
2022-05-17 21:20:17 +0900

    백준 2225(합분해, dp)

--- src/Main.java
+++ src/Main.java
@@ -3,40 +3,40 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static Long[] dp = new Long[101];
+    static Long[][] dp;
     static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int T = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
 
-        dp[0] = Long.valueOf(0);
-        dp[1] = dp[2] = dp[3] = Long.valueOf(1);
-        dp[4] = Long.valueOf(2);
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        while(T-->0){
-            int N = Integer.parseInt(br.readLine());
+        dp = new Long[K+1][N+1];
 
-            if(N >= 5)
-                wave(N);
-
-            sb.append(dp[N] + "\n");
+        for(int i = 0 ; i <= N ; i++){
+            dp[0][i] = Long.valueOf(0);
         }
 
-        System.out.println(sb);
+        sum(K,N);
+
+        System.out.println(dp[K][N]%1000000000);
 
     }
 
-    public static long wave(int n){
+    public static long sum(int a, int b){
 
-        if(dp[n] == null)
-            dp[n] = wave(n-1) + wave(n-5);
+        if(b == 0 || a==1)
+            return dp[a][b] = Long.valueOf(1);
 
-        return dp[n];
+        if(dp[a][b] == null){
+            dp[a][b] = sum(a-1,b) + sum(a,b-1);
+        }
+
+        return dp[a][b]%1000000000;
     }
-
-
-
 }
