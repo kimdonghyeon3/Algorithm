69805ab860c56e3ff2a803dcdbff8cb51bef5cda
kimdonghyeon <sh2115521@naver.com>
2022-05-22 12:02:48 +0900

    백준 10825(국영수, 정렬)

--- src/Main.java
+++ src/Main.java
@@ -14,27 +14,40 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[][] arr = new int[N][2];
-
-        String[] s = new String[N];
+        String[][] arr = new String[N][4];
 
         for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine());
-            arr[i][0] = Integer.parseInt(st.nextToken());
-            s[i] = st.nextToken();
-            arr[i][1] = i;
+            for(int j = 0 ; j < 4 ; j++){
+                arr[i][j] = st.nextToken();
+            }
         }
 
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
 
-                return o1[0]-o2[0];
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if(o1[1].equals(o2[1])){
+
+                    if(o1[2].equals(o2[2])){
+
+                        if(o1[3].equals(o2[3])){
+                            return o1[0].compareTo(o2[0]);
+                        }else{
+                            return Integer.parseInt(o2[3])-Integer.parseInt(o1[3]);
+                        }
+
+                    }else{
+                        return Integer.parseInt(o1[2])-Integer.parseInt(o2[2]);
+                    }
+                }
+
+                return Integer.parseInt(o2[1])-Integer.parseInt(o1[1]);
             }
         });
 
         for(int i = 0 ; i < N ; i++){
-            sb.append(arr[i][0] +" " + s[arr[i][1]] + "\n");
+            sb.append(arr[i][0] + "\n");
         }
 
         System.out.println(sb);
