d9f432c9478be41dfd54ac591ac389daa5516f41
kimdonghyeon <sh2115521@naver.com>
2022-05-22 11:42:14 +0900

    백준 10814(나이순 정렬, 정렬)

--- src/Main.java
+++ src/Main.java
@@ -16,24 +16,25 @@ public class Main {
 
         int[][] arr = new int[N][2];
 
+        String[] s = new String[N];
+
         for(int i = 0 ; i < N ; i++){
             st = new StringTokenizer(br.readLine());
             arr[i][0] = Integer.parseInt(st.nextToken());
-            arr[i][1] = Integer.parseInt(st.nextToken());
+            s[i] = st.nextToken();
+            arr[i][1] = i;
         }
 
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if(o1[1] == o2[1])
-                    return o1[0]-o2[0];
 
-                return o1[1]-o2[1];
+                return o1[0]-o2[0];
             }
         });
 
         for(int i = 0 ; i < N ; i++){
-            sb.append(arr[i][0] +" " + arr[i][1] + "\n");
+            sb.append(arr[i][0] +" " + s[arr[i][1]] + "\n");
         }
 
         System.out.println(sb);
