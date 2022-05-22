f63626faeb2c8962d0b38a83d64e4dbac540c2e1
kimdonghyeon <sh2115521@naver.com>
2022-05-22 14:07:51 +0900

    백준 10828 (스택, stack)

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,5 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
@@ -13,23 +10,42 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        Stack<Integer> stack = new Stack<>();
 
-        arr = new int[N];
-        st = new StringTokenizer(br.readLine());
+        while( N-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
 
-        for(int i = 0 ; i < N  ; i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+            if(st.hasMoreTokens()){
+                stack.push(Integer.parseInt(st.nextToken()));
+            }else{
+                if(command.equals("pop")){
+                    if(stack.isEmpty()){
+                        sb.append("-1" + "\n");
+                    }else{
+                        sb.append(stack.pop()+"\n");
+                    }
+                }else if(command.equals("size")){
+                    sb.append(stack.size() + "\n");
+                }else if(command.equals("empty")){
+                    if(stack.isEmpty()){
+                        sb.append("1" + "\n");
+                    }else
+                        sb.append("0" + "\n");
+                }else if(command.equals("top")) {
+                    if(stack.isEmpty()){
+                        sb.append("-1" + "\n");
+                    }else{
+                        sb.append(stack.peek() + "\n");
+                    }
+                }
+            }
+
         }
 
-        Arrays.sort(arr);
-
-        System.out.println(arr[K-1]);
-
-
+        System.out.println(sb);
     }
 
 
