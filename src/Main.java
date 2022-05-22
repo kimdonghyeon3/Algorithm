3aed87fc31b14dde72dfd0c0b2f950c6b3e9c7b3
kimdonghyeon <sh2115521@naver.com>
2022-05-22 14:55:47 +0900

    백준 10799 (쇠막대기, stack)

--- src/Main.java
+++ src/Main.java
@@ -4,46 +4,36 @@ import java.util.*;
 public class Main {
 
     static StringBuilder sb = new StringBuilder();
-    static int[] arr;
-    static boolean flag = false;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        char[] ch = br.readLine().toCharArray();
+
+        int num = 0;
 
         Stack<Character> stack = new Stack<>();
 
-        while( N-->0){
-            char[] ch = br.readLine().toCharArray();
+        for(int i = 0 ; i < ch.length ; i++){
+            if(ch[i] == '('){
+                stack.push(ch[i]);
+                continue;
+            }
+            if(ch[i] == ')'){
+                stack.pop();
 
-            for(int i = 0 ; i < ch.length ; i++){
-                if(ch[i] == '('){
-                    stack.push(ch[i]);
+                if(ch[i-1] == '('){
+                    num += stack.size();
                 }else{
-                    if(stack.isEmpty()){
-                        sb.append("NO\n");
-                        flag = true;
-                        break;
-                    }else {
-                        stack.pop();
-                        continue;
-                    }
+                    num ++;
                 }
+
             }
 
-            if(stack.isEmpty() && !flag){
-                sb.append("YES\n");
-            }else if(!stack.isEmpty() && !flag)
-                sb.append("NO\n");
-
-            flag = false;
-
-            stack.clear();
         }
 
-        System.out.println(sb);
+        System.out.println(num);
     }
 
 
