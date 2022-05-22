fcf184595867c327d5db45737930d78d3fa778c7
kimdonghyeon <sh2115521@naver.com>
2022-05-22 12:13:26 +0900

    백준 10989 (수 정렬하기3, 정렬)

--- src/Main.java
+++ src/Main.java
@@ -14,40 +14,16 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        String[][] arr = new String[N][4];
+        int[] arr = new int[N];
 
         for(int i = 0 ; i < N ; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0 ; j < 4 ; j++){
-                arr[i][j] = st.nextToken();
-            }
+            arr[i] = Integer.parseInt(br.readLine());
         }
 
-
-        Arrays.sort(arr, new Comparator<String[]>() {
-            @Override
-            public int compare(String[] o1, String[] o2) {
-                if(o1[1].equals(o2[1])){
-
-                    if(o1[2].equals(o2[2])){
-
-                        if(o1[3].equals(o2[3])){
-                            return o1[0].compareTo(o2[0]);
-                        }else{
-                            return Integer.parseInt(o2[3])-Integer.parseInt(o1[3]);
-                        }
-
-                    }else{
-                        return Integer.parseInt(o1[2])-Integer.parseInt(o2[2]);
-                    }
-                }
-
-                return Integer.parseInt(o2[1])-Integer.parseInt(o1[1]);
-            }
-        });
+        Arrays.sort(arr);
 
         for(int i = 0 ; i < N ; i++){
-            sb.append(arr[i][0] + "\n");
+            sb.append(arr[i] + "\n");
         }
 
         System.out.println(sb);
