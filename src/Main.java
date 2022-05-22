7745173ed12fdf1417336497774c11656d58d8ea
kimdonghyeon <sh2115521@naver.com>
2022-05-22 13:42:48 +0900

    백준 11004 (K번째 수, 정렬)

--- src/Main.java
+++ src/Main.java
@@ -7,40 +7,27 @@ import java.util.StringTokenizer;
 public class Main {
 
     static StringBuilder sb = new StringBuilder();
-    static long[] arr = new long[100001];
-    static long max = 0;
-    static long index = 0;
+    static int[] arr;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        HashMap<String, Integer> map = new HashMap<>();
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-        for(int i = 0 ; i < N ; i++){
-            String temp = br.readLine();
-            if(map.containsKey(temp))
-                map.replace(temp,map.get(temp)+1);
-            else
-                map.put(temp,1);
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+
+        for(int i = 0 ; i < N  ; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        for(String key : map.keySet()){
-//            System.out.println("key : " + key + " value : " + map.get(key));
-            if(max < map.get(key)){
-                max = map.get(key);
-                index = Long.parseLong(key);
+        Arrays.sort(arr);
 
-            }else if(max == map.get(key)){
-                if(index > Long.parseLong(key)) {
-                    index = Long.parseLong(key);
-                }
-            }
-        }
-
-        System.out.println(index);
+        System.out.println(arr[K-1]);
 
 
     }
