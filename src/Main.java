c2ab2c9b245b1164167e9c140019e81719352348
kimdonghyeon <sh2115521@naver.com>
2022-05-22 11:20:32 +0900

    백준 11650(좌표 정렬하기, 정렬)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,16 +14,26 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[N];
+        int[][] arr = new int[N][2];
 
         for(int i = 0 ; i < N ; i++){
-            arr[i] = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        Arrays.sort(arr);
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if(o1[0] == o2[0])
+                    return o1[1]-o2[1];
+
+                return o1[0]-o2[0];
+            }
+        });
 
         for(int i = 0 ; i < N ; i++){
-            sb.append(arr[i] + "\n");
+            sb.append(arr[i][0] +" " + arr[i][1] + "\n");
         }
 
         System.out.println(sb);
