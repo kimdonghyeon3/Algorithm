30596cba93157ef787dbc28b874aad2f4c0bb2c5
kimdonghyeon <sh2115521@naver.com>
2022-05-22 11:23:07 +0900

    백준 11651(좌표 정렬하기2, 정렬)

--- src/Main.java
+++ src/Main.java
@@ -25,10 +25,10 @@ public class Main {
         Arrays.sort(arr, new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
-                if(o1[0] == o2[0])
-                    return o1[1]-o2[1];
+                if(o1[1] == o2[1])
+                    return o1[0]-o2[0];
 
-                return o1[0]-o2[0];
+                return o1[1]-o2[1];
             }
         });
 
