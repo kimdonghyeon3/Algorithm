0208ede6ea0337c24331cb2679df93e5b970491a
kimdonghyeon <sh2115521@naver.com>
2022-05-23 15:31:39 +0900

    백준 11655 (ROT13, 문자열)

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,27 @@ public class Main {
 
         char[] ch = br.readLine().toCharArray();
 
-        System.out.println(ch.length);
+        for(int i = 0 ; i < ch.length ; i++){
+
+            int num = ch[i];
+
+            if(ch[i] >= 65 && ch[i] <= 90){
+                num += 13;
+
+                if (num > 90)
+                    num = 64 + (num - 90);
+
+            }else if(ch[i] >= 97 && ch[i] <= 122){
+                num += 13;
+
+                if (num > 122)
+                    num = 96 + (num - 122);
+            }
+                ch[i] = (char) num;
+        }
+        for(char c : ch){
+            System.out.print(c);
+        }
 
 
     }
