6700b2814990487ed3f495cf2895dc55fed62c5e
kimdonghyeon <sh2115521@naver.com>
2022-05-23 14:15:08 +0900

    백준 10845 (큐, queue)

--- src/Main.java
+++ src/Main.java
@@ -4,36 +4,59 @@ import java.util.*;
 public class Main {
 
     static StringBuilder sb = new StringBuilder();
+    static int last_num;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        char[] ch = br.readLine().toCharArray();
+        int N = Integer.parseInt(br.readLine());
 
-        int num = 0;
+        Queue<Integer> q = new LinkedList<>();
 
-        Stack<Character> stack = new Stack<>();
+        while( N-->0){
 
-        for(int i = 0 ; i < ch.length ; i++){
-            if(ch[i] == '('){
-                stack.push(ch[i]);
-                continue;
-            }
-            if(ch[i] == ')'){
-                stack.pop();
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-                if(ch[i-1] == '('){
-                    num += stack.size();
-                }else{
-                    num ++;
+            String command = st.nextToken();
+
+            if(st.hasMoreTokens()){
+                last_num = Integer.parseInt(st.nextToken());
+                q.add(last_num);
+            }else{
+
+                if(command.equals("pop")){
+                    if(q.isEmpty()){
+                        sb.append("-1\n");
+                    }else{
+                        sb.append(q.poll() + "\n");
+                    }
+                }else if(command.equals("size")){
+                    sb.append(q.size() + "\n");
+                }else if(command.equals("empty")){
+                    if(q.isEmpty()){
+                        sb.append("1\n");
+                    }else{
+                        sb.append("0\n");
+                    }
+                }else if(command.equals("front")){
+                    if(q.isEmpty())
+                        sb.append("-1\n");
+                    else
+                        sb.append(q.peek() + "\n");
+                }else if(command.equals("back")){
+                    if(q.isEmpty())
+                        sb.append("-1\n");
+                    else
+                        sb.append(last_num + "\n");
                 }
-
             }
 
         }
 
-        System.out.println(num);
+
+
+        System.out.println(sb);
     }
 
 
