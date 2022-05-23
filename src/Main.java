eb6d0fcae08c18006c040c86f769a89c86ac0423
kimdonghyeon <sh2115521@naver.com>
2022-05-23 15:17:46 +0900

    백준 2743 (단어 길이 재기, 문자열)

--- src/Main.java
+++ src/Main.java
@@ -9,29 +9,10 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = "";
+        char[] ch = br.readLine().toCharArray();
 
-        while((str=br.readLine()) != null){
+        System.out.println(ch.length);
 
-            char[] ch = str.toCharArray();
-            int[] arr = new int[4];
-
-            for(int i = 0 ; i < ch.length ; i++){
-
-                int ascii = (int)ch[i];
-                if(ascii == 32){
-                    arr[3] += 1;
-                }else if( ascii >= 48 && ascii <= 57){
-                    arr[2] += 1;
-                }else if(ascii >= 65 && ascii <= 90 ){
-                    arr[1] += 1;
-                }else{
-                    arr[0] += 1;
-                }
-            }
-
-            System.out.println(arr[0] + " " + arr[1] + " " + arr[2] + " " + arr[3]);
-        }
 
     }
 
