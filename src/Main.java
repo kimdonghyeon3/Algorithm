a57f370002140f4c0cf6a319e74a498752980b13
kimdonghyeon <sh2115521@naver.com>
2022-05-31 17:38:28 +0900

    백준 1158 (나머지, 수학/구현/사칙연산)

--- src/Main.java
+++ src/Main.java
@@ -10,32 +10,15 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> q = new LinkedList<>();
+        sb.append( (A+B)%C + "\n");
+        sb.append( ((A%C) + (B%C))%C + "\n");
+        sb.append( (A*B)%C + "\n");
+        sb.append( ((A%C)*(B%C))%C + "\n");
 
-        for(int i = 1 ; i <= N ; i++){
-            q.add(i);
-        }
-
-        int cnt = 1;
-
-        sb.append("<");
-
-        while(!q.isEmpty()){
-
-            if(cnt == K){
-                sb.append(q.poll() + ", ");
-                cnt = 1;
-            }else{
-                q.add(q.poll());
-                cnt++;
-            }
-        }
-
-        sb.setLength(sb.length()-2);
-        sb.append(">");
 
         System.out.println(sb);
     }
