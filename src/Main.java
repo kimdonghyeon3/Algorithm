1ebac2e86f42511ac8be512f4827a8997e0e7454
kimdonghyeon <sh2115521@naver.com>
2022-06-29 13:06:41 +0900

    백준 9613 (GCD합, 유클리드 호제법) *int범위 조심

--- src/Main.java
+++ src/Main.java
@@ -9,39 +9,43 @@ public class Main {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int T = Integer.parseInt(br.readLine());
 
+        while(T-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
+            int arr[] = new int[n];
 
-        long a = Long.parseLong(st.nextToken());
-        long b = Long.parseLong(st.nextToken());
+            for(int i = 0 ; i < arr.length ; i++){
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
 
-        long result = max(a,b);
+            long sum = 0;
 
-
-        for(int i = 1 ; i <= result ; i++)
-            sb.append("1");
+            for(int i = 0 ; i < arr.length ; i++){
+                for(int j = i+1 ; j < arr.length ; j++){
+                    sum += gcd(arr[i],arr[j]);
+                }
+            }
+            sb.append(sum + "\n");
+        }
 
         System.out.println(sb);
-
     }
 
-    public static long max(long A, long B){
+    public static int gcd(int a, int b){
+        int max = a >= b ? a : b;
+        int min = a >= b ? b : a;
+        int temp;
 
-        long max = Math.max(A,B);
-        long min = Math.min(A,B);
-        long result = 1;
         while(min != 0){
-            result = max % min;
+            temp = max % min;
             max = min;
-            min = result;
+            min = temp;
         }
+
         return max;
-    }
-
-    public static long min(int a, int b){
-
-        return (a*b)/max(a, b);
     }
 }
 
