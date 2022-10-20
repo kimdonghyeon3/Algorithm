bfb5bffc6f68563885fdbb66abfbce40afd3d383
kimdonghyeon3 <sh2115521@naver.com>
2022-10-21 01:11:46 +0900

    feat : 문자열 찾기

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,25 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args){
 
-    static StringBuilder sb = new StringBuilder();
+        Scanner in=new Scanner(System.in);
 
-    public static void main(String[] args) throws Exception {
+        String str1 = in.next();
+        char str2 = in.next().charAt(0);
 
-        int str1 = 'A';
-        int str2 = 'B';
+        str1 = str1.toLowerCase();
+        str2 = Character.toLowerCase(str2);
 
-        String ans = "";
-        //ff
+        int cnt = 0;
 
+        for(int i = 0 ; i < str1.length() ; i++){
+            if(str1.charAt(i) == str2)
+                cnt++;
+        }
 
-        ans += 'A';
+        System.out.println(cnt);
 
-        HashMap<Character, Integer> map = new HashMap<>();
-
-        char mbti = 'c';
-        map.put(mbti,1);
-
-        System.out.print(str1);
-        System.out.print('A' < 'B');
+        return ;
     }
-
 }
-
-
-
-
-
