0dbcf04988c675ca4c63edbdca89db1ee771b260
kimdonghyeon3 <sh2115521@naver.com>
2022-10-21 01:54:00 +0900

    feat : 단어뒤집기 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,19 @@ public class Main {
     public static void main(String[] args){
 
         Scanner in=new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int N = in.nextInt();
 
-        String str1 = in.next();
-
-        String[] arr = str1.split(" ");
-
-        int max = 0;
-        String ans = "";
-
-        for(String str = arr){
-            if(max < str.length()){
-                ans = str;
-                max = str.length();
+        while(N-->0){
+            String str = in.next();
+            String ans = "";
+            for(int i = str.length()-1 ; i >= 0 ; i--){
+                sb.append(str.charAt(i));
             }
+            sb.append("\n");
         }
 
-
-        System.out.println(ans);
+        System.out.println(sb);
 
         return ;
     }
