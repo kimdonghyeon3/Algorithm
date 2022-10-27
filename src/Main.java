c506154a5657c0b01bb91b37d69f2abdb7e83f00
kimdonghyeon3 <sh2115521@naver.com>
2022-10-27 15:47:03 +0900

    feat : 뒤집은 소수 알고리즘

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.List;
+
 import java.util.Scanner;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args){
@@ -10,19 +8,25 @@ public class Main {
 
         int N = Integer.parseInt(in.nextLine());
 
-        int ans = 0;
+        StringBuilder ans = new StringBuilder();
 
-        for(int i=2; i<=N; i++){
-            // prime[i]가 소수라면
-            if(isPrime(i)){
-                ans++;
+        for(int i = 0 ; i < N ; i++){
+            StringBuilder sb = new StringBuilder();
+            int num = Integer.parseInt(sb.append(in.next()).reverse().toString());
+
+            if(isPrime(num)){
+                ans.append(num + " ");
             }
+
         }
 
         System.out.println(ans);
     }
 
     private static boolean isPrime(int num) {
+
+        if(num <= 1)
+            return false;
 
         for (int i = 2; i<=(int)Math.sqrt(num); i++) {
             if (num % i == 0) {
